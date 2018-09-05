b5a1aae83633cedaf7c6a21d3f726bd2b47d1b53
Ray Chan <ray25222@gmail.com>
2018-09-06 00:45:49 +0800

    Draw snake on canvas with static data

--- index.js
+++ index.js
@@ -5,9 +5,59 @@ const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight - 77;
 canvas.width = window.innerWidth;
 
+const gridWidth = 5;
+const boxSize = 30;
+const headLocation = {
+  x: 200,
+  y: 200,
+};
+
+// d - down, u - up, l - left, r - right
+const linksFromHead = ['d', 'l', 'l', 'u', 'l'];
+const foodLocation = {
+  x: 480,
+  y: 480,
+};
+
 ctx.beginPath();
-ctx.fillStyle = 'palevioletred';
-ctx.fillRect(200, 200, 20, 20);
-ctx.fillRect(225, 225, 20, 20);
-ctx.fillRect(200, 225, 20, 20);
-ctx.fillRect(225, 200, 20, 20);
+
+// snake head
+ctx.fillStyle = 'steelblue';
+ctx.fillRect(headLocation.x, headLocation.y, boxSize, boxSize);
+
+// snake body
+const stepSize = (boxSize + gridWidth);
+let { x: bodyX, y: bodyY } = headLocation;
+
+ctx.fillStyle = 'cadetblue';
+
+linksFromHead.forEach(direction => {
+  switch (direction) {
+  case 'l':
+    bodyX -= stepSize;
+    break;
+
+  case 'r':
+    bodyX += stepSize;
+    break;
+
+  case 'u':
+    bodyY -= stepSize;
+    break;
+
+  case 'd':
+    bodyY += stepSize;
+    break;
+
+  default:
+    throw new Error('Invalid data type');
+  }
+
+  ctx.fillRect(bodyX, bodyY, boxSize, boxSize);
+});
+
+// food
+ctx.fillStyle = 'crimson';
+ctx.fillRect(foodLocation.x, foodLocation.y, boxSize, boxSize);
+
+console.log(headLocation)
